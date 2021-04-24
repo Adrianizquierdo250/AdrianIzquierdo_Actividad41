d6e0ac7bc89801b58775e34021acf19a9b367cda
Adrii <Adrii@192.168.1.42>
2021-04-25 01:06:00 +0200

    Third commit

--- Actividad4.1/src/cambio1/Cambio1.java
+++ Actividad4.1/src/cambio1/Cambio1.java
@@ -2,6 +2,6 @@ package cambio1;
 
 public class Cambio1 {
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo Cambio Git 1");
+		System.out.println("Hola Mundo Cambio Git 1 1");
 	}
 }
---
+++ Actividad4.1/src/cambio2/Cambio2.java
@@ -0,0 +1,9 @@
+package cambio2;
+
+public class Cambio2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo Cambio Git 2");
+	}
+
+}
